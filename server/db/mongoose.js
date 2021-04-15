ae5d769109ab14d50b3683050e6739d0921c2be2
mostafasobh <mostafasobh33@gmail.com>
2021-04-15 16:44:55 +0200

    still modifying /users rout

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const pass = process.env.DB_PASSWORD
 const dbName = 'TodoApp'
 const mongoose = require('mongoose');
 const db = mongoose.connection
-const url = `mongodb+srv://todoAppDB:${pass}@cluster0.qzpgn.mongodb.net/${dbName}?retryWrites=true&w=majority`
+const url = `mongodb+srv://todoAppDB:${pass}@cluster0.qzpgn.mongodb.net/TodoApp?retryWrites=true&w=majority`
 // mongoose.Promise = global.Promise
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, });
 
--- server/server.js
+++ server/server.js
@@ -104,12 +104,11 @@ app.post('/todos', authenticate, async (req, res) => {
 app.post('/users', (req, res) => {
     let body = _.pick(req.body, ['email', 'password'])
     let user = new User(body)
-    res.send({ user, msg: 'not able to reach database' })
-    // user.save().then(() => {
-    //     return user.generateAuthToken()
-    // }).then((token) => {
-    //     res.header('x-auth', token).send(user)
-    // }).catch(e => res.status(400).send({ message: 'you couldnt reach the database', e }))
+    user.save().then(() => {
+        return user.generateAuthToken()
+    }).then((token) => {
+        res.header('x-auth', token).send(user)
+    }).catch(e => res.status(400).send(e))
 })
 //====================================================================//
 
